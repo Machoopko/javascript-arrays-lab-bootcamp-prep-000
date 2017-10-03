f3a16553f216d99a7849d180a872dee39a2fc422
Marina Kosovic <marinakosovic15@gmail.com>
2017-10-03 10:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 
 kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten('Ralph') {
+function destructivelyAppendKitten("Ralph") {
   kittens.push("Ralph");
 }

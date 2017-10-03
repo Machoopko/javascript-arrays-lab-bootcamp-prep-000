2b76703309ce756195af43479c3a1b030d78a653
Marina Kosovic <marinakosovic15@gmail.com>
2017-10-03 10:58:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   kittens.push("Ralph");
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
